b3c730076f0017b0946e5b1b549ae6663b5d52d8
Thomas Phillips <thomasphillips3@gmail.com>
2017-01-06 20:49:54 -0500

    Created UI and connected outlets and actions.

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -9,17 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    @IBAction func showNextQuestion(sender: AnyObject) {
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func showAnswer(sender: AnyObject) {
+        
     }
-
 
 }
 
